8387d5ed16b9bf269d282374558b230108bb8605
zxxia2016 <zxxia2016@gmail.com>
2022-06-14 09:50:40 +0800

    Return and handle an error

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,10 +1,14 @@
 package greetings
 
 import (
+	"errors"
 	"fmt"
 )
 
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	if name == "" {
+		return name, errors.New("empty name")
+	}
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
--- hello/hello.go
+++ hello/hello.go
@@ -1,3 +1,5 @@
+// demo v1 Hello World
+
 // $ go mod init example/hello
 // $ go run .
 // $ go help
@@ -7,12 +9,20 @@
 // $ go env -w GOPROXY=https://goproxy.cn,direct 设置代理，否则包无法下载
 // search for a "quote" package. https://pkg.go.dev/search?q=quote
 
+// demo v2: Create a Go module
+
+// $ go mod edit -replace example.com/greetings=../greetings 替换本地依赖
+// $ go mod tidy 同步依赖
+
+// demo v3: Return and handle an error
+
 // TODO: https://go.dev/doc/tutorial/create-module
 
 package main
 
 import (
 	"fmt"
+	"log"
 
 	"rsc.io/quote"
 
@@ -23,6 +33,9 @@ func main() {
 	fmt.Println("Hello, World!")
 	fmt.Println(quote.Go())
 
-	message := greetings.Hello("Gladys")
+	message, errors := greetings.Hello("")
+	if errors != nil {
+		log.Fatal(errors)
+	}
 	fmt.Println(message)
 }
